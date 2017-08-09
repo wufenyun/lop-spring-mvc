2416dc3c19f8b262ed311c3bece882078034da91
wufenyun <wufenyun@zbj.com>
2017-08-09 20:47:25 +0800

    schema test

---
+++ src/main/java/com/lop/demo/controller/user/LopController.java
@@ -0,0 +1,23 @@
+/**
+ * Package: com.lop.demo.controller.user
+ * Description: 
+ */
+package com.lop.demo.controller.user;
+
+import com.lopframework.lop.annotation.ServiceProcessor;
+import com.lopframework.lop.annotation.ServiceProcessorMethod;
+
+/**
+ * Description:  
+ * Date: 2017年8月9日 下午3:09:46
+ * @author wufenyun 
+ */
+@ServiceProcessor
+public class LopController {
+    
+    @ServiceProcessorMethod(method = "hello", version = "1.0")
+    public Object hello() {
+        System.out.println("hello service call");
+        return "hello";
+    }
+}
---
+++ src/test/java/com/lop/test/TestSpringSchema.java
@@ -0,0 +1,24 @@
+package com.lop.test;
+
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.lopframework.lop.servlet.AnnotationServiceDispatcher;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class TestSpringSchema {
+
+    @Test
+    public void test() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+
+        AnnotationServiceDispatcher ds = (AnnotationServiceDispatcher) context.getBean("annotationServiceDispatcher");
+        ApplicationContext applicationContext = ds.getApplicationContext();
+        System.out.println(applicationContext.getId());
+    }
+}
